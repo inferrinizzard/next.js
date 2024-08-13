06c97d0916a74e82f40336ef96f4f36fc2700e8d
Tobias Koppers <tobias.koppers@googlemail.com>
2024-08-13 07:19:45 +0200

    WIP: remove todos

--- turbopack/crates/turbo-tasks-backend/src/backend/mod.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/mod.rs
@@ -1090,7 +1090,7 @@ impl Backend for TurboTasksBackend {
         _: TaskId,
         _: &dyn TurboTasksBackendApi<Self>,
     ) -> AutoMap<RawVc, i32, BuildHasherDefault<FxHasher>, 1> {
-        todo!()
+        Default::default()
     }
 
     fn emit_collectible(
@@ -1100,7 +1100,7 @@ impl Backend for TurboTasksBackend {
         _: TaskId,
         _: &dyn TurboTasksBackendApi<Self>,
     ) {
-        todo!()
+        // todo!("emit_collectible");
     }
 
     fn unemit_collectible(
@@ -1111,7 +1111,7 @@ impl Backend for TurboTasksBackend {
         _: TaskId,
         _: &dyn TurboTasksBackendApi<Self>,
     ) {
-        todo!()
+        // todo!("emit_collectible");
     }
 
     fn update_task_cell(
@@ -1169,7 +1169,8 @@ impl Backend for TurboTasksBackend {
         }
         task_id
     }
+
     fn dispose_root_task(&self, _: TaskId, _: &dyn TurboTasksBackendApi<Self>) {
-        todo!()
+        // todo!("dispose_root_task");
     }
 }
